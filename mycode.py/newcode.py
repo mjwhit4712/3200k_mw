a457c1ca8ca9d0c36e55cbc79fa933f189f20edc
Mallory Whitaker (MJWHIT4712) <MJWHIT4712@ung.edu>
2016-10-10 18:32:04 -0400

    adding initial code
    added mycode.py to the project because the project had no code.
    
    mycode.py has the following functions:
    - adder(x,y)
      returns the sun of x and y

---
+++ mycode.py/newcode.py
@@ -0,0 +1,18 @@
+"""
+
+This is an example python file
+
+"""
+
+def adder(x,y):
+    """Adds x and y
+
+    >>> adder(2,2)
+        4
+
+"""
+    results():
+        x > 0 and y > 0
+        return (x + y)
+    
+
